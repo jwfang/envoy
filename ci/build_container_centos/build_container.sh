2b7d18dbf5e2eddc8602111a615dcd210928932b
jwfang <jwfang@github>
2017-05-25 19:26:50 +0800

    scl enable git29/python27

--- ci/build_container_centos/build_container.sh
+++ ci/build_container_centos/build_container.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 yum install -y rh-git29-git golang libtool make openssl wget time python27-python-pip \
-               unzip java-1.8.0-openjdk-devel which cmake devtoolset-4-libatomic-devel
+               unzip java-1.8.0-openjdk-devel which cmake devtoolset-4-libatomic-devel \
+               clang
 
 # bazel
 wget https://people.centos.org/tru/bazel-centos7/bazel-0.4.5-1.el7.centos.x86_64.rpm && \
   rpm -ivh bazel-0.4.5-1.el7.centos.x86_64.rpm && rm bazel-0.4.5-1.el7.centos.x86_64.rpm
+
+# enable git29/pip, need set +e
+set +e
+source /usr/bin/scl_source enable rh-git29
+source /usr/bin/scl_source enable python27
+set -e
 
 # virtualenv
 pip install virtualenv
@@ -22,11 +29,6 @@ if [[ "${CXX_VERSION}" != "g++ (GCC) 5.3.1 20160406 (Red Hat 5.3.1-6)" ]]; then
   echo "Unexpected compiler version: ${CXX_VERSION}"
   exit 1
 fi
-
-# enable git29, need set +e
-set +e
-source /usr/bin/scl_source enable rh-git29
-set -e
 
 export THIRDPARTY_DEPS=/tmp
 export THIRDPARTY_SRC=/thirdparty
